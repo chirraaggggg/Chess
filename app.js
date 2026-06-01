23864393aa48c2fcd4c352474251a0a4a4b213be
Chirag <chhirag920@gmail.com>
2026-06-01 17:56:20 +0530

    pushing the code

--- app.js
+++ app.js
@@ -31,10 +31,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 // routes
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Chess Game" })
-})
-app.get('/landing', (req, res) => {
+app.get('/', (req, res) => {
   res.render('landing')
 })
 app.get('/login', (req, res) => {
